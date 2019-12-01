c7b49ad263a5b8a2716c8f5646b4d8d040da0168
Yuhao Zhang <yhzhang1994@gmail.com>
2019-11-30 20:41:32 -0800

    fix python3.6

--- dependencies.sh
+++ dependencies.sh
@@ -5,4 +5,5 @@ sudo apt-get -y install libffi-dev libssl-dev python-dev
 sudo apt-get -y install python-pip
 sudo pip install --upgrade pip
 sudo pip install os_client_config
+sudo pip install --ignore-installed pycparser==2.17 enum34==1.1.6 wrapt==1.10.8 simplejson==3.10.0
 sudo pip install -r pip-list.txt
